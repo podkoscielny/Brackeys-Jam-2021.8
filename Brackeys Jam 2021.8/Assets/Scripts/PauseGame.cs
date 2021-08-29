2c6deb74a2f29cf30ad26a9354d8057604e97666
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2021-08-29 04:54:10 +0200

    Add pausing game

---
+++ Brackeys Jam 2021.8/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    [SerializeField] GameObject pauseText;
+
+    private bool _isGamePaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            if (_isGamePaused)
+            {
+                Time.timeScale = 1;
+                pauseText.SetActive(false);
+            }
+            else
+            {
+                Time.timeScale = 0;
+                pauseText.SetActive(true);
+            }
+
+            _isGamePaused = !_isGamePaused;
+        }
+    }
+}
