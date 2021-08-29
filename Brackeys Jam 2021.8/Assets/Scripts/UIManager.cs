5a0fb4a5e1da273f63171042ff305e41cdbea09b
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2021-08-29 03:08:41 +0200

    Add chaos stars UI

--- Brackeys Jam 2021.8/Assets/Scripts/UIManager.cs
+++ Brackeys Jam 2021.8/Assets/Scripts/UIManager.cs
@@ -6,10 +6,21 @@ using TMPro;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] GameObject[] chaosStars;
 
-    void OnEnable() => GameManager.OnScoreUpdated += UpdateScore;
+    void OnEnable()
+    {
+        GameManager.OnScoreUpdated += UpdateScore;
+        GameManager.OnChaosStarGained += EnableChaosStar;
+    }
 
-    void OnDisable() => GameManager.OnScoreUpdated -= UpdateScore;
+    void OnDisable()
+    {
+        GameManager.OnScoreUpdated -= UpdateScore;
+        GameManager.OnChaosStarGained -= EnableChaosStar;
+    }
 
     void UpdateScore(int score) => scoreText.text = score.ToString();
+
+    void EnableChaosStar(int chaosStarsAmount) => chaosStars[chaosStarsAmount - 1].SetActive(true);
 }
