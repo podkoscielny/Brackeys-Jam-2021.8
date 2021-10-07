54d4f82109bb801ae2bc7a14fb89091b9226c95e
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2021-10-07 14:35:34 +0200

    Rename ScriptsDisabler to ScriptsSwitcher

--- Brackeys Jam 2021.8/Assets/Scripts/ScriptsSwticher.cs
+++ Brackeys Jam 2021.8/Assets/Scripts/ScriptsSwticher.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScriptsDisabler : MonoBehaviour
+public class ScriptsSwticher : MonoBehaviour
 {
     [SerializeField] List<MonoBehaviour> scriptsToDisable;
 
