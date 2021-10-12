4e79712856d3aaf48633a9c07cb1099f4268f954
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2021-10-12 16:31:42 +0200

    Add force to player on hit taken

--- Brackeys Jam 2021.8/Assets/Scripts/Player/PlayerCollisions.cs
+++ Brackeys Jam 2021.8/Assets/Scripts/Player/PlayerCollisions.cs
@@ -8,9 +8,14 @@ public class PlayerCollisions : MonoBehaviour
 
     private GameManager _gameManager;
     private Rigidbody2D _playerRb;
-    private const float HIT_FORCE = 20f;
+    private Collider2D _playerCollider;
+    private const float HIT_FORCE = 10f;
 
-    void Awake() => _playerRb = GetComponent<Rigidbody2D>();
+    void Awake()
+    {
+        _playerRb = GetComponent<Rigidbody2D>();
+        _playerCollider = GetComponent<Collider2D>();
+    }
 
     void OnEnable() => GameManager.OnGameOver += DisableCollisionsWithCharacters;
 
@@ -18,24 +23,34 @@ public class PlayerCollisions : MonoBehaviour
 
     private void Start() => _gameManager = GameManager.Instance;
 
-
     void DisableCollisionsWithCharacters() => Physics2D.IgnoreLayerCollision(9, 8);
 
 
-    void OnTriggerEnter2D(Collider2D collision) => PushThePlayerOnCollision(collision.tag, collision.transform.position);
-
-    void OnCollisionEnter2D(Collision2D collision) => PushThePlayerOnCollision(collision.collider.tag, collision.transform.position);
-
-
-    void PushThePlayerOnCollision(string tag, Vector3 position)
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        if (damageableTags.Contains(tag))
-        {
-            Vector2 direction = transform.position - position;
-            _playerRb.velocity = Vector2.zero;
-            _playerRb.AddForce(direction * HIT_FORCE, ForceMode2D.Impulse); // add specific force to specific objects
+        //if (damageableTags.Contains(collision.tag))
+        //{
+        //    Vector2 direction = _colliderCenter - collision.bounds.center;
+        //    PushThePlayerOnCollision(direction);
+        //}
+    }
 
-            _gameManager.GetHit(0.5f); // add specific amount to specific objects
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (damageableTags.Contains(collision.collider.tag))
+        {
+            Debug.Log(collision.collider.bounds.center);
+            Vector2 direction = (_playerCollider.bounds.center - collision.collider.bounds.center).normalized;
+            PushThePlayerOnCollision(direction);
         }
+    }
+
+    void PushThePlayerOnCollision(Vector3 direction)
+    {
+        //Debug.Log(direction);
+        _playerRb.velocity = Vector2.zero;
+        _playerRb.AddForce(direction * HIT_FORCE, ForceMode2D.Impulse); // add specific force to specific objects
+
+        _gameManager.GetHit(0.5f); // add specific amount to specific objects
     }
 }
