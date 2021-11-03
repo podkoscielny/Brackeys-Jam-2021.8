3ac6fe848edb2361ed670c4886000f37c331b590
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2021-11-03 21:56:53 +0100

    Make background parallax effect

---
+++ Brackeys Jam 2021.8/Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    [SerializeField] GameObject mainCamera;
+    [SerializeField] float parallaxEffect;
+
+    private float _startPosition;
+
+    void Start()
+    {
+        _startPosition = transform.position.x;
+    }
+
+    void LateUpdate() => CalculateParallax();
+
+    void CalculateParallax()
+    {
+        float distance = mainCamera.transform.position.x * parallaxEffect;
+
+        transform.position = new Vector3(_startPosition + distance, transform.position.y, transform.position.z);
+    }
+}
