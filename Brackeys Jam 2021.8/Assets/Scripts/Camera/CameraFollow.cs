a4379cf4f803460e4af35ae3d45250758f224e53
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2022-03-28 17:15:54 +0200

    Adjust camera follow position

--- Brackeys Jam 2021.8/Assets/Scripts/Camera/CameraFollow.cs
+++ Brackeys Jam 2021.8/Assets/Scripts/Camera/CameraFollow.cs
@@ -24,23 +24,30 @@ public class CameraFollow : MonoBehaviour
 
     private float CalculateLensSize()
     {
-        #if UNITY_ANDROID
-        return Mathf.SmoothDamp(virtualCamera.m_Lens.OrthographicSize, player.position.y * 0.23816f + 3.7635f, ref _cameraLensVelocityRef, SMOOTH_TIME);
-        #else
+#if UNITY_ANDROID
+        return Mathf.SmoothDamp(virtualCamera.m_Lens.OrthographicSize, CalculateLinearFunctionValue(0.23f, 3.25f), ref _cameraLensVelocityRef, SMOOTH_TIME);
+#else
         return Mathf.SmoothDamp(virtualCamera.m_Lens.OrthographicSize, player.position.y * 0.2943f + 4.5057f, ref _cameraLensVelocityRef, SMOOTH_TIME);
-        #endif
+#endif
     }
 
     private Vector3 CalculateCameraOffset()
     {
         float offsetY;
 
-        #if UNITY_ANDROID
-        offsetY = Mathf.SmoothDamp(_cameraTransposer.m_TrackedObjectOffset.y, player.position.y * (-0.60526f) - 0.42288f, ref _cameraOffsetVelocityRef, SMOOTH_TIME);
-        #else
+#if UNITY_ANDROID
+        offsetY = Mathf.SmoothDamp(_cameraTransposer.m_TrackedObjectOffset.y, CalculateLinearFunctionValue(-0.62f, -0.59f), ref _cameraOffsetVelocityRef, SMOOTH_TIME);
+#else
         offsetY = Mathf.SmoothDamp(_cameraTransposer.m_TrackedObjectOffset.y, player.position.y * (-0.6724f) + 0.3392f, ref _cameraOffsetVelocityRef, SMOOTH_TIME);
-        #endif
+#endif
 
         return new Vector3(0, offsetY, 0);
+    }
+
+    private float CalculateLinearFunctionValue(float a, float b)
+    {
+        float x = player.position.y > 3 ? 3 : player.position.y;
+
+        return x * a + b;
     }
 }
