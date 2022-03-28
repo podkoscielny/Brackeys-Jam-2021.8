087f9ffe8daaa7d947dda74e60f720d1d41d76c0
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2022-03-28 13:56:43 +0200

    Adjust mobile UI size

--- Brackeys Jam 2021.8/Assets/Scripts/Player/PlayerMovement.cs
+++ Brackeys Jam 2021.8/Assets/Scripts/Player/PlayerMovement.cs
@@ -18,7 +18,7 @@ public class PlayerMovement : MonoBehaviour
     private bool _canMove = false;
     private float _horizontalMovement = 0f;
 
-    private const float JOYSTICK_SENSITIVITY = 0.4f;
+    private const float JOYSTICK_SENSITIVITY = 0.2f;
     private const float MOVEMENT_SPEED = 5f;
     private const float SHOOT_DELAY = 0.5f;
     private const float MOVEMENT_DELAY = 0.7f;
