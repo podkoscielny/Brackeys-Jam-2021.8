2d814a4046f71aad55e1d30a1f054ef5a28367e0
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2022-04-21 20:56:00 +0200

    Add player footstep sound

---
+++ Brackeys Jam 2021.8/Assets/Scripts/Player/PlayerAudio.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    [SerializeField] AudioSource playerAudio;
+    [SerializeField] AudioClip footstepSoundEffect;
+
+    public void PlayFootstepSound() //Invoke in walk animation
+    {
+        playerAudio.clip = footstepSoundEffect;
+        playerAudio.Play();
+    }
+}
